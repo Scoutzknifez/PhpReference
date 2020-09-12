407faffa7deb2cfcf568bc87c6400b1ae5804516
Scoutzknifez <cody@4alexanders.com>
2020-09-12 01:24:36 -0700

    Pushed a slight change

--- test.php
+++ test.php
@@ -7,6 +7,7 @@ $sub = new MathProblem(10, 5, ProblemType::SUBTRACTION);
 $mult = new MathProblem(3, 8, ProblemType::MULTIPLICATION);
 $div = new MathProblem(36, 6, ProblemType::DIVISION);
 
+echo "<header>Template</header>";
 echo $add->print();
 echo "<br>";
 echo $sub->print();
