40af41d98b1fce5e2c0711b364e9fe0634f01f34
Scoutzknifez <codygaming@4alexanders.com>
2020-09-23 23:09:25 -0700

    Fixed the PUT api call, made it transmit data if needed

--- utility/Utils.php
+++ utility/Utils.php
@@ -10,13 +10,17 @@ function CallAPI($method, $url, $data = false)
     switch ($method)
     {
         case "POST":
-            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POST, true);
 
             if ($data)
                 curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
             break;
         case "PUT":
-            curl_setopt($curl, CURLOPT_PUT, 1);
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+            //curl_setopt($curl, CURLOPT_PUT, true);
+            
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
             break;
         default:
             if ($data)
@@ -28,7 +32,7 @@ function CallAPI($method, $url, $data = false)
     curl_setopt($curl, CURLOPT_USERPWD, "username:password");
 
     curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
     $result = curl_exec($curl);
 
